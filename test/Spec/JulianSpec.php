eb9aead05546ac18e37c75ab2ffd05c6d2eedfd7
Kyle <kylekatarnls@users.noreply.github.com>
2022-08-08 09:29:17 +0200

    Add more tests
    Co-authored-by: Mathias STRASSER <roukmoute@users.noreply.github.com>

--- test/Spec/JulianSpec.php
+++ test/Spec/JulianSpec.php
@@ -18,6 +18,11 @@ class JulianSpec extends ObjectBehavior
     {
         $this->jdtogregorian(2458489)->shouldReturn('1/5/2019');
         $this->jdtogregorian(2458465)->shouldReturn('12/12/2018');
+        $this->jdtogregorian(0)->shouldReturn('0/0/0');
+        $this->jdtogregorian(1)->shouldReturn('11/25/-4714');
+        $this->jdtogregorian(2816423)->shouldReturn('1/1/2999');
+        $this->jdtogregorian(536838866)->shouldReturn('10/17/1465102');
+        $this->jdtogregorian(536838867)->shouldReturn('0/0/0');
     }
 
     public function it_calculates_julian_day_count_from_jewish_date()
